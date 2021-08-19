d099184b9bb69b3507ddc0c536967b2ef57daa54
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-08-18 23:58:02 -0500

    chore: Database location variable was modified to be more generic

--- src/util/JDBCUtilities.java
+++ src/util/JDBCUtilities.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCUtilities {
-    private static final String DATABASE_LOCATION = "db/ProyectosConstruccion.db";
+    private static final String DATABASE_FILEPATH = "db/";
+    private static final String DATABASE_FILENAME = "ProyectosConstruccion.db";
+    private static final String DATABASE_LOCATION = DATABASE_FILEPATH + DATABASE_FILENAME;
 
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:sqlite:" + DATABASE_LOCATION;
