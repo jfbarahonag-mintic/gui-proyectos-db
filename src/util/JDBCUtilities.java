4627410cfe0d960b2c6c50720894c5b3e743990e
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-08-18 23:50:45 -0500

    feat: all needed to establish a connection with the database was added

---
+++ src/util/JDBCUtilities.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCUtilities {
+    private static final String DATABASE_LOCATION = "db/ProyectosConstruccion.db";
+
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:sqlite:" + DATABASE_LOCATION;
+
+        return DriverManager.getConnection(url);
+    }
+}
