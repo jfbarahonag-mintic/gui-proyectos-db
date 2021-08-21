9833700144cd129f7948891fd8df7fffb7e607c0
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-08-20 23:22:00 -0500

    VO of insert leader option was added

---
+++ src/model/VO/insertLeaderVO.java
@@ -0,0 +1,76 @@
+package model.VO;
+
+public class insertLeaderVO {
+    
+    private Integer ID;
+    private Integer salario;
+    private Integer clasificacion;
+    private String nombre;
+    private String primerapellido;
+    private String segundoApellido;
+    private String ciudadResidencia;
+    private String cargo;
+    private String documentoIdentidad;
+    private String fechaNacimiento;
+    
+    public Integer getID() {
+        return ID;
+    }
+    public void setID(Integer iD) {
+        ID = iD;
+    }
+    public Integer getSalario() {
+        return salario;
+    }
+    public void setSalario(Integer salario) {
+        this.salario = salario;
+    }
+    public Integer getClasificacion() {
+        return clasificacion;
+    }
+    public void setClasificacion(Integer clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPrimerapellido() {
+        return primerapellido;
+    }
+    public void setPrimerapellido(String primerapellido) {
+        this.primerapellido = primerapellido;
+    }
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+    public String getCiudadResidencia() {
+        return ciudadResidencia;
+    }
+    public void setCiudadResidencia(String ciudadResidencia) {
+        this.ciudadResidencia = ciudadResidencia;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
