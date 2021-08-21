7a889a9946a38ff7b6fe7dfd7c15cc884f433476
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-08-20 21:57:58 -0500

    feat: Principal window was added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import view.MainWindow;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        MainWindow mw = new MainWindow();
+        mw.execute();
     }
 }
---
+++ src/view/MainWindow.java
@@ -0,0 +1,55 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainWindow extends JFrame implements ActionListener {
+    
+    private JButton btnInsert_, btnQuery_;
+    private JTable tblResults_;
+    private JScrollPane scrlResults_;
+
+    private FlowLayout screen_;
+
+    public MainWindow()
+    {
+        setTitle("Proyectos");
+        
+        setBounds(0, 0, 300, 100);
+        setLocationRelativeTo(null);
+        
+        btnQuery_ = new JButton("Consultar");
+        btnQuery_.setBounds(10, 10, 115, 25);
+        btnQuery_.addActionListener(this);
+        add(btnQuery_);
+        
+        btnInsert_ = new JButton("Agregar Lider");
+        btnInsert_.setBounds(10, 10, 115, 25);
+        btnInsert_.addActionListener(this);
+        add(btnInsert_);
+
+        screen_ = new FlowLayout(FlowLayout.CENTER, 5, 20);
+        setLayout(screen_);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void execute()
+    {
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource() == btnInsert_) {
+            System.out.println("Insert");
+        }
+        if (e.getSource() == btnQuery_) {
+            System.out.println("Query");
+        }
+    }
+}
